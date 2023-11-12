574e0186b074cea6506667ba7b048b67f11e6825
heosujinnn <0204sujin@naver.com>
2023-11-12 16:49:03 +0900

    [add] setState 메서드로 위젯 업데이트 하기

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -6,11 +6,15 @@ class MainScreen extends StatefulWidget {
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
+/// stless
+/// alt+enter -> remove 할 수 있음.
+/// alt+enter -> wrap with Row ... 등 기능 추가도 가능!
+/// ctrl+alt+L : 자동정렬
+/// ctrl + .  : 코드 접기
 
 class _MainScreenState extends State<MainScreen> {
-
-  List lstHello = ['허수진','ㅎㅇ','안녕','팅후루'] ;
-  TextEditingController idController=TextEditingController();
+  TextEditingController idController = TextEditingController();
+  String msg = '이 곳에 입력값이 업데이트 됩니다.';
 
   @override
   Widget build(BuildContext context) {
@@ -18,21 +22,25 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: const Text('메인화면'),
       ),
-      // stless
-      // alt+enter -> remove 할 수 있음.
-      // alt+enter -> wrap with Row ... 등 기능 추가도 가능!
-      // ctrl+alt+L : 자동정렬
-      // ctrl + .  : 코드 접기
       body: Column(
         children: [
           TextField(
             controller: idController,
             decoration: InputDecoration(labelText: '아이디를 입력해주세요'),
           ),
-          ElevatedButton(onPressed: (){
-            // 클릭 시 동작을 구현한다.
-            print(idController.text.toString());
-          }, child: Text('아이디 입력 값을 가져온다.'))
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                // widget 업데이트!! setState 안에 넣는것이 좋습니다. 값만 리로딩되니까 성능적으로 굿
+                msg = idController.text.toString();
+              });
+            },
+            child: Text('아이디 입력 값을 가져온다.'),
+          ),
+          Text(
+            msg,
+            style: TextStyle(fontSize: 30),
+          )
         ],
       ),
     );
